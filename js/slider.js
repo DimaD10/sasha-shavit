d5e1d55be4a5b30c4288e3b8b173b6e40b594fe4
Дима Дзюбенко <dzubenkod7@gmail.com>
2023-06-12 22:45:23 +0300

    edit quiz for non-scrolling

--- js/slider.js
+++ js/slider.js
@@ -1,21 +1,3 @@
-const filter = new Swiper('.filter-slider', {
-    loop: true,
-    slidesPerView: 'auto',
-    spaceBetween: 20,
-    autoplay: true,
-    speed: 600,
-
-    breakpoints: {
-        // when window width is >= 320px
-        320: {
-          spaceBetween: 10
-        },
-        // when window width is >= 480px
-        480: {
-          spaceBetween: 20
-        }
-      }
-});
 
 const featured = new Swiper('.featured-slider', {
     loop: true,
